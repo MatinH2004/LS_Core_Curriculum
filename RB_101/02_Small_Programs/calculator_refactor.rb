5617c35cb89fbbf552dea15f958954ae4aea32f9
Matin Hassan Pour <matinhp04@gmail.com>
2022-12-05 00:54:47 -0800

    refactored/updated calculator_refactor.rb

--- RB_101/02_Small_Programs/calculator_refactor.rb
+++ RB_101/02_Small_Programs/calculator_refactor.rb
@@ -1,7 +1,10 @@
 require 'yaml'
+require 'pry'
 MESSAGES = YAML.load_file('calculator_messages.yml')
-language = 'en'
+@language = 'en'
+# instance variable, so methods can access it
 
+# default language: english
 def messages(message, lang='en')
   MESSAGES[lang][message]
 end
@@ -10,30 +13,32 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+# remove decimal if number is whole
+class Float
+  def prettify
+    to_i == self ? to_i : self
+  end
+end
+
+# validate user number
 def number?(num)
-  if num.to_i == num
-    num.to_i
-    true
-  elsif num.to_f == num
-    num.to_f
-    true
+  if num.to_i() == num
+    num
+  elsif num.to_f() == num
+    num
   else
     false
   end
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
-  op
+  operation = case op
+              when '1' then prompt(messages('add_nums', @language))
+              when '2' then prompt(messages('subtract_nums', @language))
+              when '3' then prompt(messages('multiply_nums', @language))
+              when '4' then prompt(messages('divide_nums', @language))
+              end
+  operation
 end
 
 prompt(MESSAGES['en']['choose_lang'])
@@ -43,69 +48,68 @@ language_pref = ''
 loop do
   language_pref = Kernel.gets().chomp()
   if language_pref.downcase() == 'en'
-    language = 'en'
+    @language = 'en'
     break
   elsif language_pref.downcase() == 'fr'
-    language = 'fr'
+    @language = 'fr'
     break
   end
-  prompt('invalid_lang')
+  prompt(messages('invalid_lang', @language))
 end
 
-prompt(messages('welcome', language))
+prompt(messages('welcome', @language))
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
 
   if name.empty?()
-    prompt(messages('invalid_name', language))
+    prompt(messages('invalid_name', @language))
   else
     break
   end
 end
 
-prompt(messages('greeting', language) + "#{name}!")
+prompt(messages('greeting', @language) + "#{name}!")
 
 loop do # main loop
   number1 = ''
   loop do
-    prompt(messages('first_number', language))
+    prompt(messages('first_number', @language))
     number1 = Kernel.gets().chomp()
 
     if number?(number1.to_f)
       break
     else
-      prompt(messages('invalid_name', language))
+      prompt(messages('invalid_name', @language))
     end
   end
 
   number2 = ''
   loop do
-    prompt(messages('second_number', language))
+    prompt(messages('second_number', @language))
     number2 = Kernel.gets().chomp()
 
     if number?(number2.to_f)
       break
     else
-      prompt(messages('invalid_number', language))
+      prompt(messages('invalid_number', @language))
     end
   end
 
-  prompt(messages('operator', language))
+  prompt(messages('operator', @language))
 
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
 
     if %w(1 2 3 4).include?(operator)
+      operation_to_message(operator)
       break
     else
-      prompt(messages('choose_operator', language))
+      prompt(messages('choose_operator', @language))
     end
   end
-
-  prompt(operation_to_message(operator).to_s() + messages('numbers', language))
 
   result = case operator
            when '1' then number1.to_f() + number2.to_f()
@@ -114,11 +118,11 @@ loop do # main loop
            when '4' then number1.to_f() / number2.to_f()
            end
 
-  prompt(messages('result', language) + result.to_s())
+  prompt(messages('result', @language) + result.prettify.to_s())
 
-  prompt(messages('calculate_again', language))
+  prompt(messages('calculate_again', @language))
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
 
-prompt(messages('goodbye', language) + "#{name}!")
+prompt(messages('goodbye', @language) + "#{name}!")
