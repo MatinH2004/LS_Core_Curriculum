19bd95868f139e1101515be850a28d997ef57d42
Matin Hassan Pour <matinhp04@gmail.com>
2023-02-10 10:52:34 -0800

    refactor + update pedac

--- Code_Wars/typoglycemia_generator.rb
+++ Code_Wars/typoglycemia_generator.rb
@@ -3,101 +3,71 @@
 # -------------------------- Problem
   Restate the problem:
 
-  Write a method that takes a string, and sorts the letters alphabetically 
-  between the first and last letters.
-
-  Explicit:
-    - the first and last letters must remain in original places
-    - characters between first and last characters must be sorted alphabetically
-    - punctuation should remain at the same place as started, ie. shan't -> sahn't
-
-  Implicit:
-    - return string if size is 0 to 3.
-    - string can have multiple words.
+  Given a string return a new string with
+    - the first and last chars the same as the original
+    - characters between the first and last characters must be sorted alphabetically
+    - punctuation must remain in the same positions
 
   Input: string of word(s)
-  Output: inside of words alphabetically sorted.
+  Output: string of words, each word[1..-2] chars sorted alphabetically
 
 # -------------------------- Test Cases
 
-describe("Basic tests") do
-Test.assert_equals(scramble_words('professionals'), 'paefilnoorsss', 'The first and last letters of a word should reamin in place with the inner letters sorted')
-Test.assert_equals(scramble_words('i'), 'i', 'Must handle single charater words')
-Test.assert_equals(scramble_words(''), '', 'Must handle empty strings')
-Test.assert_equals(scramble_words('me'), 'me', 'Must handle 2 charater words')
-Test.assert_equals(scramble_words('you'), 'you', 'Must handle 3 charater words')
-Test.assert_equals(scramble_words('card-carrying'), 'caac-dinrrryg', 'Only spaces separate words and punctuation should remain at the same place as it started')
-Test.assert_equals(scramble_words("shan't"), "sahn't", 'Punctuation should remain at the same place as it started')
-Test.assert_equals(scramble_words('-dcba'), '-dbca', 'Must handle special character at the start')
-Test.assert_equals(scramble_words('dcba.'), 'dbca.', 'Must handle special character at the end')
-Test.assert_equals(scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth."), "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.", 'Must handle a full sentence')
-end
+  - Seperate words by single spaces
+  - special chars at the beginning/end of word do not take position of alphas
+    i.e. -dcba => -dbca
+  - only special chars => [- ' , .]
+  - ignore capitalization
 
 # -------------------------- Data Structure
 
-
-
-# -------------------------- Scrapbook
-
-map
-chars
-slice
+  Array: to get letters and sort them
+  Hash: to handle special chars
 
 # -------------------------- Algorithm
 
-/* given a string of words */
-- split string into seperate words
-- while iterating thru words, get all characters except first and last
-- sort characters alphabetically unless character is non-alphabetical
+  /* given a string of words */
+    - For each word
+      - store the index of special characters in a hash
+        - iterate over the characters with index,
+        - if character is not alphabetical (a-z)
+          - save character as key in hash and its index as the value
+      - remove the special characters
+
+      - if there's only one special character and one letter
+        return the word
+
+      - get the first letter
+      - get the last letter
+      - take middle characters, sort them alphabetically
+      - new_str is first + sorted + last
+      - insert special chars
 
 =end
-require 'pry'
-
-# def scramble_words(words)
-#   return words if words.length <= 3
-#   words.split.map do |word|
-#     word[0] + word[1..-2].chars.sort.join + word[-1]
-#   end.join(' ')
-# end
 
 def scramble_words(words)
-  return words if words.length <= 3
+  words.split(' ').map do |word|
+      if word.size < 3
+        word 
+      else
+        hash = {}
+        0.upto(word.size - 1) do |idx|
+          char = word[idx]
+          hash[char] = idx if char =~ /[^a-z]/i
+        end
 
-  words.split.map do |word|
-    first = word[0]
-    last = word[-1]
+        word = word.gsub(/[^a-z]/i, "")
+        sorted_middle = word[1..-2].chars.sort.join
+        new_word = word[0] + sorted_middle + word[-1]
 
-    if first =~ /[^a-z]/i
-      until first =~ /[a-z]/i
-      first = word[(0.succ)]
+        hash.each_pair do |k, v|
+          new_word.insert(v, k)
+        end
+
+        new_word
       end
-    elsif last =~ /[^a-z]/i
-      until last =~ /[a-z]/i
-        last = word[-(1.succ)]
-      end
-    end
-
-    middle = word[(word.index(first)+1)..(word.index(last)-1)]
-
-    if word == "you'll" # couldn't find a way to deal with this edge case
-      middle = word[(word.index(first)+1)..(word.index(last))]
-    end
-
-    non_alpha = word.chars.keep_if {|x| x =~ /[^a-z]/i}[0]
-    non_alpha_idx = word.index(non_alpha) if !non_alpha.nil?
-
-    # binding.pry
-
-    if non_alpha.nil?
-      first + word[1..-2].chars.sort.join + last
-    elsif middle.gsub!(/[^a-z]/, '').nil?
-      (first + middle.chars.sort.join + last).insert(non_alpha_idx, non_alpha)
-    else
-      first + middle.gsub(/[^a-z]/, '').chars.sort.insert(non_alpha_idx - 1, non_alpha).join + last
-    end
   end.join(' ')
 end
-
 
 # abcdefghijklmnopqrstuvwxyz
 
@@ -111,5 +81,41 @@ p scramble_words("shan't") == "sahn't"
 p scramble_words('-dcba') == '-dbca'
 p scramble_words('dcba.') == 'dbca.' 
 p scramble_words("you'll") == "ylo'ul"
-# p scramble_words("you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.")
-                #  "you've gotta dacne like teehr's nbdooy wachintg, love like you'l neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth."
+p scramble_words("you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.")
+
+
+# def scramble_words(words)
+#   return words if words.length <= 3
+
+#   words.split.map do |word|
+#     first = word[0]
+#     last = word[-1]
+
+#     if first =~ /[^a-z]/i
+#       until first =~ /[a-z]/i
+#       first = word[(0.succ)]
+#       end
+#     elsif last =~ /[^a-z]/i
+#       until last =~ /[a-z]/i
+#         last = word[-(1.succ)]
+#       end
+#     end
+
+#     middle = word[(word.index(first)+1)..(word.index(last)-1)]
+
+#     if word == "you'll" # couldn't find a way to deal with this edge case
+#       middle = word[(word.index(first)+1)..(word.index(last))]
+#     end
+
+#     non_alpha = word.chars.keep_if {|x| x =~ /[^a-z]/i}[0]
+#     non_alpha_idx = word.index(non_alpha) if !non_alpha.nil?
+
+#     if non_alpha.nil?
+#       first + word[1..-2].chars.sort.join + last
+#     elsif middle.gsub!(/[^a-z]/, '').nil?
+#       (first + middle.chars.sort.join + last).insert(non_alpha_idx, non_alpha)
+#     else
+#       first + middle.gsub(/[^a-z]/, '').chars.sort.insert(non_alpha_idx - 1, non_alpha).join + last
+#     end
+#   end.join(' ')
+# end
