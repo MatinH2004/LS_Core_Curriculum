267759d307d6a1a2724a1baf86d7ba9d3c5bddaf
Matin Hassan Pour <matinhp04@gmail.com>
2023-02-12 13:26:24 -0800

    fixed all edge cases

--- Code_Wars/top_3_words.rb
+++ Code_Wars/top_3_words.rb
@@ -61,3 +61,16 @@ def top_3_words(text)
     arr[0]
   end
 end
+
+p top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") == ["e", "ddd", "aa"] 
+p top_3_words(" //wont won't won't ") == ["won't", "wont"]
+p top_3_words(" , e .. ") == ["e"]
+p top_3_words(" ... ") == []
+p top_3_words(" ' ") == []
+p top_3_words(" ''' ") == []
+p top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.""") == ["a", "of", "on"]
