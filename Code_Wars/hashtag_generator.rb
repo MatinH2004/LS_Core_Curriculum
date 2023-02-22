e37aa4e8430377a7f30edad32728e2f349f4d33b
Matin Hassan Pour <matinhp04@gmail.com>
2023-02-21 21:16:10 -0800

    solved codewars

---
+++ Code_Wars/hashtag_generator.rb
@@ -0,0 +1,44 @@
+=begin
+
+P: Write a method that takes a string and turns it
+   into a hashtag. First letter of every word must be
+   capitalized. If final result is longer than 140 chars,
+   or if input string is empty: return false
+
+E: - capitalize words
+   - return false if input is empty
+   - return false if output string larger than 140 chars
+
+I: - none identified
+
+T: " Hello there thanks for trying my Kata" => "#HelloThereThanksForTryingMyKata" 
+   " Hello World " => "#HelloWorld"
+   "" => false
+
+A: /* given a string */
+   - return false if str is empty
+   - initialize result variable as '#'
+   - split string by every word
+     - iterate through array
+     - capitalize every word and append to result string
+   - return false if length of result is greater than 140
+
+=end
+
+# def generate_hashtag(str)
+#   return false if str.strip.empty?
+#   result = '#'
+#   str.split.each {|x| result += x.capitalize}
+#   return false if result.size > 140
+#   result
+# end
+
+# refactor
+def generate_hashtag(str)
+  res = str.split.map {|x| x.capitalize}.prepend('#').join
+  return false if str.strip.empty? || res.size > 140; res
+end
+
+p generate_hashtag(" Hello there thanks for trying my Kata") == "#HelloThereThanksForTryingMyKata"
+p generate_hashtag(" Hello World ") == "#HelloWorld"
+p generate_hashtag("") == false
