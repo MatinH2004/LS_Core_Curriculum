6c77323dd5696212cd80aba0e04fd161f4d0ffda
Matin Hassan Pour <matinhp04@gmail.com>
2023-03-01 23:47:38 -0800

    updated notes

--- RB_110/01_Ruby_Collections/loop_practice/multiplier.rb
+++ RB_110/01_Ruby_Collections/loop_practice/multiplier.rb
@@ -1,16 +1,45 @@
-def multiply(array, multiply_by)
-  multiplied_numbers = []
+# mutate original array
+def multiply!(numbers, multiplier)
   counter = 0
 
   loop do
-    break if counter == array.size
-
-    multiplied_numbers << array[counter] * multiply_by
+    break if counter == numbers.size
+    numbers[counter] *= multiplier
     counter += 1
   end
 
-  multiplied_numbers
+  numbers
 end
 
-my_numbers = [1, 4, 3, 7, 2, 6]
-p multiply(my_numbers, 3) # => [3, 12, 9, 21, 6, 18]
+# 2.
+def multiply_map!(numbers, multiplier)
+  numbers.map! {|n| n * multiplier}
+end
+
+# return a new array
+
+# 1.
+def multiply(numbers, multiplier)
+  multiplied_nums = []
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+    multiplied_nums << numbers[counter] * multiplier
+    counter += 1
+  end
+
+  multiplied_nums
+end
+
+# 2.
+def multiply_map(numbers, multiplier)
+  numbers.map {|n| n * multiplier}
+end
+
+numbers = [1,2,3,4,5]
+
+p multiply(numbers, 2)
+p multiply_map(numbers, 2)
+p multiply!(numbers, 2)
+p multiply_map!(numbers, 2)
