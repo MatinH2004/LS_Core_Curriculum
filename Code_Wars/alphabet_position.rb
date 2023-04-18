5f66ea911618918796cb63b25c78fa0488640672
Matin Hassan Pour <matinhp04@gmail.com>
2023-04-18 15:01:42 -0700

    updated solutions

--- Code_Wars/alphabet_position.rb
+++ Code_Wars/alphabet_position.rb
@@ -50,3 +50,18 @@ def alphabet_position(str)
 end
 
 p alphabet_position("The sunset sets at twelve o' clock.") == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+# new solution
+
+def alphabet_position(str)
+  alphabet = ('a'..'z').zip(1..26).to_h
+  str.downcase.chars.map { |c| alphabet[c] if c =~ /[a-z]/i }.compact.join(' ')
+end
+
+# one liner
+
+def alphabet_position(str)
+  str.downcase.delete('^a-zA-z').bytes.map {|c| c - 96}.join(' ')
+end
+
+# Note: String#bytes returns an array of integers representing the characters ascii values.
