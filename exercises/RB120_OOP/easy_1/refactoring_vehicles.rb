de82a4947be3b52a85a1b86b7e0b9536e768fa01
Matin Hassan Pour <matinhp04@gmail.com>
2023-05-04 18:52:56 -0700

    updated solution

--- exercises/RB120_OOP/easy_1/refactoring_vehicles.rb
+++ exercises/RB120_OOP/easy_1/refactoring_vehicles.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   attr_reader :make, :model
 
   def initialize(make, model)
@@ -6,46 +6,87 @@ class Car
     @model = model
   end
 
+  def to_s
+    "#{make} #{model}"
+  end
+end
+
+class Car < Vehicle
   def wheels
     4
   end
-
-  def to_s
-    "#{make} #{model}"
-  end
 end
 
-class Motorcycle
-  attr_reader :make, :model
-
-  def initialize(make, model)
-    @make = make
-    @model = model
-  end
-
+class Motorcycle < Vehicle
   def wheels
     2
   end
-
-  def to_s
-    "#{make} #{model}"
-  end
 end
 
-class Truck
-  attr_reader :make, :model, :payload
+class Truck < Vehicle
+  attr_reader :payload
 
   def initialize(make, model, payload)
-    @make = make
-    @model = model
+    super(make, model)
     @payload = payload
   end
 
   def wheels
     6
   end
+end
+
+# Further Exploration
+
+# some vehicle objects may not have wheels, ie planes
+# so it would make sense to init a constant with the number
+# of wheels the object has, and call the constant when the method
+# is invoked.
+
+# nick's amazing solution
+
+class Vehicle
+  attr_reader :make, :model, :wheels
+
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
 
   def to_s
     "#{make} #{model}"
   end
+
+  def wheels
+    self.class::WHEELS
+  end
 end
+
+class Car < Vehicle
+  WHEELS = 4
+end
+
+class Motorcycle < Vehicle
+  WHEELS = 2
+end
+
+class Truck < Vehicle
+  WHEELS = 4
+
+  attr_reader :payload
+
+  def initialize(make, model, payload)
+    super(make, model)
+    @payload = payload
+  end
+end
+
+class Plane < Vehicle
+  WHEELS = 0
+end
+
+mytruck = Truck.new("RAM", "3500", "100kg")
+puts mytruck.wheels # => 4
+
+myplane = Plane.new("Boeing", "737")
+puts myplane.wheels # => 0
