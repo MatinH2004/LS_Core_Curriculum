2e1044e7819b6c0f6fcc9679fa0f7fb294fa8531
Matin Hassan Pour <matinhp04@gmail.com>
2023-05-10 13:42:14 -0700

    added AI personalities

--- RB_120/02_Object_Oriented_Programming/object_oriented_rps/oo_rps_bonus.rb
+++ RB_120/02_Object_Oriented_Programming/object_oriented_rps/oo_rps_bonus.rb
@@ -34,8 +34,8 @@ module Displayable
   end
 
   def display_moves
-    puts "\n#{human.name} chose #{human.move}."
-    puts "#{computer.name} chose #{computer.move}."
+    puts "\n#{human.name} chose #{human.show_move}."
+    puts "#{computer.name} chose #{computer.show_move}."
   end
   
   def display_score
@@ -55,20 +55,7 @@ module Displayable
     puts "\n#{winner.name} is the grand winner!"
   end
 
-  def determine_winner
-    human_move = human.move
-    computer_move = computer.move
-
-    if human_move > computer_move
-      human.update_score
-      puts "\n#{human.name} won!"
-    elsif human_move < computer_move
-      computer.update_score
-      puts "\n#{computer.name} won!"
-    else
-      puts "\nIt's a tie!"
-    end
-  end
+  
 end
 
 class Score
@@ -110,11 +97,11 @@ class Move
   end
 
   def >(other_move)
-    VALUES[value][:beats].include?(other_move)
+    VALUES[@value][:beats].include?(other_move.value)
   end
 
   def <(other_move)
-    VALUES[other_move][:beats].include?(value)
+    VALUES[other_move.value][:beats].include?(@value)
   end
 
   def to_s
@@ -142,13 +129,20 @@ class Player
     @score.value
   end
 
-  def move
+  def show_move
     @move.value
   end
 end
 
 class Human < Player
   include Displayable
+
+  @@human_move = ''
+
+  def self.move
+    @@human_move
+  end
+
   def set_name
     system('clear') || system('cls')
     n = nil
@@ -171,6 +165,7 @@ class Human < Player
       prompt_invalid
     end
     self.move = Move.new(valid_move?(choice))
+    @@human_move = self.move
   end
 
   def search_by_abbr(choice)
@@ -199,12 +194,37 @@ class Human < Player
 end
 
 class Computer < Player
+  def choose
+    self.move = Move.new(Move::VALUES.keys.sample)
+  end
+end
+
+class Rocky < Computer    # always chooses rock
   def set_name
-    self.name = ['Ahmed', 'Atefeh', 'Saeed', 'Abdul Hossein'].sample
+    @name = 'Rocky'
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.keys.sample)
+    self.move = Move.new('rock')
+  end
+end
+
+class ChatGPT < Computer  # beats you every time
+  def set_name
+    @name = 'ChatGPT'
+  end
+
+  def choose
+    value = Move::VALUES.keys.select do |key|
+      Move::VALUES[key][:beats].include?(Human.move.value)
+    end.sample
+    self.move = Move.new(value)
+  end
+end
+
+class Siri < Computer     # regular bot
+  def set_name
+    @name = 'Siri'
   end
 end
 
@@ -212,12 +232,12 @@ class RPSGame
   include Displayable
   attr_accessor :human, :computer
 
-  WIN_SCORE = 1
+  WIN_SCORE = 3
   RULES = File.open('oo_rps_text.txt', 'r')
 
   def initialize
     @human = Human.new
-    @computer = Computer.new
+    @computer = ChatGPT.new#[Rocky.new, ChatGPT.new, Siri.new].sample
   end
   
   def play
@@ -247,13 +267,29 @@ class RPSGame
     system('clear') || system('cls')
   end
 
-  def game_over?
-    [human, computer].any? { |player| player.score == WIN_SCORE}
-  end
-
   def players_choose
     human.choose
     computer.choose
+  end
+
+  def determine_winner
+    human_move = human.move
+    computer_move = computer.move
+
+    if human_move > computer_move
+      human.update_score
+      puts "\n#{human.name} won!"
+    elsif human_move < computer_move
+      computer.update_score
+      puts "\n#{computer.name} won!"
+    else
+      puts "\nIt's a tie!"
+    end
+  end
+  
+
+  def game_over?
+    [human, computer].any? { |player| player.score == WIN_SCORE}
   end
 
   def play_again?
