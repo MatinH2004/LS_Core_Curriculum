316c39a2cc9ba19e5ec199aa0bfdae71a8eeff07
Matin Hassan Pour <matinhp04@gmail.com>
2023-08-06 11:37:29 -0700

    minitest notes

--- RB_130/02_Testing/car_test.rb
+++ RB_130/02_Testing/car_test.rb
@@ -5,14 +5,57 @@ Minitest::Reporters.use!
 require_relative 'car'
 
 class CarTest < Minitest::Test
-  def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+  def setup
+    @car = Car.new
   end
 
-  def test_bad_wheels
-    skip("TEST SKIPPED LOL")
-    car = Car.new
-    assert_equal(3, car.wheels)
+  def test_car_exists
+    assert(@car)
+  end
+
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
+
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")
+    end
+  end
+
+  def test_instance_of_car
+    assert_instance_of(Car, @car)
+  end
+
+  def test_includes_car
+    arr = [1, 2, 3]
+    arr << @car
+
+    assert_includes(arr, @car)
+  end
+
+  def teardown
+    # executed after tests are run
   end
 end
+
+=begin
+
+  *** SEAT APPROACH ***:
+
+1. Set up necessary objects
+2. Execute the code against the objects we're testing
+3. Assert that the executed code did the right thing
+4. Teardown and clean up any lingering artifacts
+
+- The #setup method will be called before running tests
+  - for instantiating objects, etc.
+
+- The #teardown method will be called after running tests
+  - for cleaning up files, logging info, or closing db connections
+
+=end
