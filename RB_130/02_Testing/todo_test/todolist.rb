04f6b5c126f8a7bde3bd859340e87c35f51d55e3
Matin Hassan Pour <matinhp04@gmail.com>
2023-08-06 14:24:07 -0700

    wrote test suite for todo list

---
+++ RB_130/02_Testing/todo_test/todolist.rb
@@ -0,0 +1,147 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError, "Can only add Todo objects" if todo.class != Todo
+    todos.push(todo)
+  end
+
+  alias_method :<<, :add
+
+  def size
+    todos.length
+  end
+
+  def first
+    todos[0]
+  end
+
+  def last
+    todos[-1]
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all?(&:done?)
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    todos.each(&:done!)
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete(item_at(index))
+  end
+
+  def to_s
+    <<~STR.chomp
+    ---- #{title} ----
+    #{todos.join("\n")}
+    STR
+  end
+
+  def each
+    todos.each { |todo| yield(todo) }
+    self
+  end
+
+  def select
+    todos.each_with_object(TodoList.new(title)) do |todo, list|
+      list << todo if yield(todo)
+    end
+  end
+
+  def find_by_title(title)
+    select { |todo| todo == title }.first
+  end
+
+  def all_done
+    select(&:done?)
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each(&:done!)
+  end
+
+  def mark_all_undone
+    each(&:undone!)
+  end
+
+  private
+
+  attr_reader :todos
+end
+
+list = TodoList.new('name')
+p list.method(:add) == list.method(:<<)
---
+++ RB_130/02_Testing/todo_test/todolist_test.rb
@@ -0,0 +1,163 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'todolist'
+
+class TodoListTest < Minitest::Test
+
+  def setup
+    @todo1 = Todo.new("Buy milk")
+    @todo2 = Todo.new("Clean room")
+    @todo3 = Todo.new("Go to gym")
+    @todos = [@todo1, @todo2, @todo3]
+
+    @list = TodoList.new("Today's Todos")
+    @list.add(@todo1)
+    @list.add(@todo2)
+    @list.add(@todo3)
+  end
+
+  def test_to_a
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_size
+    assert_equal(3, @list.size)
+  end
+
+  def test_first
+    assert_equal(@todo1, @list.first)
+  end
+
+  def test_last
+    assert_equal(@todo3, @list.last)
+  end
+
+  def test_shift
+    todo = @list.shift
+    assert_equal(@todo1, todo)
+    assert_equal([@todo2, @todo3], @list.to_a)
+  end
+
+  def test_pop
+    todo = @list.pop
+    assert_equal(@todo3, todo)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
+
+  def test_done?
+    assert_equal(false, @list.done?)
+  end
+
+  def test_verify_type_error
+    assert_raises(TypeError) { @list.add('todo') }
+    assert_raises(TypeError) { @list.add(1) }
+  end
+
+  def test_shovel_method
+    new_todo = Todo.new("Do oil change")
+    @list << new_todo
+    @todos << new_todo
+
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_alias_methods
+    assert_equal(@list.method(:add), @list.method(:<<))
+  end
+
+  def test_item_at
+    assert_raises(IndexError) { @list.item_at(100) }
+    assert_equal(@todo1, @list.item_at(0))
+    assert_equal(@todo2, @list.item_at(1))
+  end
+
+  def test_mark_done_at
+    assert_raises(IndexError) { @list.mark_done_at(100) }
+    @list.mark_done_at(0)
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(false, @todo3.done?)
+  end
+
+  def test_mark_undone_at
+    assert_raises(IndexError) { @list.mark_undone_at(100) }
+
+    [@todo1, @todo2, @todo3].each(&:done!)
+    @list.mark_undone_at(1)
+
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_mark_all_done
+    @list.done!
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+    assert_equal(true, @list.done?)
+  end
+
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(100) }
+    @list.remove_at(1)
+    assert_equal([@todo1, @todo3], @list.to_a)
+  end
+
+  def test_to_s_1
+    output = <<~OUTPUT.chomp
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [ ] Clean room
+    [ ] Go to gym
+    OUTPUT
+  
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_to_s_2
+    output = <<~OUTPUT.chomp
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [X] Clean room
+    [ ] Go to gym
+    OUTPUT
+
+    @list.mark_done_at(1)
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_to_s_3
+    output = <<~OUTPUT.chomp
+    ---- Today's Todos ----
+    [X] Buy milk
+    [X] Clean room
+    [X] Go to gym
+    OUTPUT
+
+    @list.done!
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_each
+    result = []
+    @list.each { |todo| result << todo }
+    assert_equal([@todo1, @todo2, @todo3], result)
+  end
+
+  def test_each_return_value
+    assert_equal(@list, @list.each { |todo| nil })
+  end
+
+  def test_select
+    @list.mark_done_at(2)
+    list = TodoList.new(@list.title)
+    list.add(@todo3)
+
+    assert_equal(list.title, @list.title)
+    assert_equal(list.to_s, @list.select(&:done?).to_s)
+  end
+  
+end
