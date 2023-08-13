ae62f33da4f8e099cc57bb024c82ed9edc3bf5de
Matin Hassan Pour <matinhp04@gmail.com>
2023-08-13 12:43:57 -0700

    easy 1 exercises

---
+++ exercises/RB130_foundations/easy_1/06.rb
@@ -0,0 +1,12 @@
+def any?(arr)
+  arr.each { |item| return true if yield(item) }
+  false
+end
+
+p any?([1, 3, 5, 6]) { |value| value.even? } == true
+p any?([1, 3, 5, 7]) { |value| value.even? } == false
+p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p any?([1, 3, 5, 7]) { |value| true } == true
+p any?([1, 3, 5, 7]) { |value| false } == false
+p any?([]) { |value| true } == false
---
+++ exercises/RB130_foundations/easy_1/08.rb
@@ -0,0 +1,12 @@
+def none?(collection)
+  collection.each { |obj| return false if yield(obj) }
+  true
+end
+
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
