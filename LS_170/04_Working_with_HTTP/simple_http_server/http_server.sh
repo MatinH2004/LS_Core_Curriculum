f835ac835240b916173743b77d0cffdd71e54e91
Matin Hassan Pour <matinhp04@gmail.com>
2023-09-28 10:46:55 -0700

    http server

---
+++ LS_170/04_Working_with_HTTP/simple_http_server/http_server.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function server () {
+  while true
+  do
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    if [[ $method = 'GET' ]]
+    then
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
+    fi
+  done
+}
+
+coproc SERVER_PROCESS { server; }
+
+nc -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
