9b0714f6e6288e6ad6ec96599a1b22deef1257c3
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-25 23:32:12 -0700

    counter

---
+++ LS_175/02_Handling_Requests_Manually/counter.rb
@@ -0,0 +1,49 @@
+require "socket"
+
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = (params || "").split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
+server = TCPServer.new("localhost", 3003)
+
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+  puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Counter</h1>"
+
+  number = params["number"].to_i
+
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+  client.puts "</body>"
+  client.puts "</html>"
+
+  client.close
+end
