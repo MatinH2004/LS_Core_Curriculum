e622910cce2549d5416ac30abfa54cc570375ee3
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-26 13:05:47 -0700

    refactored by creating #response method

--- LS_175/03_Working_with_Sinatra/my_framework/app.rb
+++ LS_175/03_Working_with_Sinatra/my_framework/app.rb
@@ -4,20 +4,24 @@ class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {'Content-Type' => 'text/html'}, [erb(:index)]]
+      status = '200'
+      headers = {"Content-Type" => "text/html"}
+      response(status, headers) do
+        erb :index
+      end
     when '/advice'
       piece_of_advice = Advice.new.generate
-      [
-        '200',
-        {'Content-Type' => 'text/html'},
-        [erb(:advice, message: piece_of_advice)]
-      ]
+      status = '200'
+      headers = {"Content-Type" => "text/html"}
+      response(status, headers) do
+        erb :advice, message: piece_of_advice
+      end
     else
-      [
-        '404',
-        {"Content-Type" => 'text/html', "Content-Length" => '48'},
-        [erb(:not_found)]
-      ]
+      status = '404'
+      headers = {"Content-Type" => "text/html", "Content-Length" => '61'}
+      response(status, headers) do
+        erb :not_found
+      end
     end
   end
 
@@ -28,6 +32,11 @@ class HelloWorld
     message = local[:message]
     content = File.read("views/#{filename}.erb")
     ERB.new(content).result(b)
+  end
+
+  def response(status, headers, body = '')
+    body = yield if block_given?
+    [status, headers, [body]]
   end
 end
 
