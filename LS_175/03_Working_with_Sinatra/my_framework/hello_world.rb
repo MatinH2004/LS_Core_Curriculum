b69a50acf08cc32dff28ce3995fd6010f65c56e0
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-26 12:49:29 -0700

    refactor #call method

---
+++ LS_175/03_Working_with_Sinatra/my_framework/hello_world.rb
@@ -0,0 +1,32 @@
+require_relative 'advice'
+
+class HelloWorld
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {'Content-Type' => 'text/html'}, [erb(:index)]]
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      [
+        '200',
+        {'Content-Type' => 'text/html'},
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/html', "Content-Length" => '48'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
+      ]
+    end
+  end
+
+  private
+
+  def erb(filename)
+    content = File.read("views/#{filename}.erb")
+    ERB.new(content).result
+  end
+end
+
+# bundle exec rackup config.ru -p 9595
