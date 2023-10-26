01535b6c2e192f52da0808a1d31025c00be29f0b
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-25 22:58:35 -0700

    parsing path and params

---
+++ LS_175/02_Handling_Requests_Manually/parsing_path_and_params.rb
@@ -0,0 +1,50 @@
+require "socket"
+
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
+server = TCPServer.new("localhost", 3003)
+
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+  puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Rolls!</h1>"
+  rolls = params["rolls"].to_i
+  sides = params["sides"].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts "<p>", roll, "</p>"
+  end
+
+  client.puts "</body>"
+  client.puts "</html>"
+
+  client.close
+end
