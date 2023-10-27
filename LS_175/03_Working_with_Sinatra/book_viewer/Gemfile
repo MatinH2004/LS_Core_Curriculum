8ad37318b8f83f853ebaab35ff98beddcf85be1b
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-27 14:16:03 -0700

    begin book viewer project

---
+++ LS_175/03_Working_with_Sinatra/book_viewer/Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem "erubis"
+gem "webrick"
+
+ruby "3.2.0"
---
+++ LS_175/03_Working_with_Sinatra/book_viewer/book_viewer.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
+end
