6aa18a918c5f05a93ded734af8c444b93747958e
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-28 13:11:18 -0700

    added view helper and redirect path

--- LS_175/03_Working_with_Sinatra/book_viewer/book_viewer.rb
+++ LS_175/03_Working_with_Sinatra/book_viewer/book_viewer.rb
@@ -2,16 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+
+end
+
+not_found do
+  redirect "/"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
