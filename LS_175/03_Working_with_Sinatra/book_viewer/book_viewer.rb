083a8e2f922c18c3e537e09a5502b59830ce9811
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-28 12:59:07 -0700

    added layout

--- LS_175/03_Working_with_Sinatra/book_viewer/book_viewer.rb
+++ LS_175/03_Working_with_Sinatra/book_viewer/book_viewer.rb
@@ -9,10 +9,14 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
+get "/chapters/:number" do
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
