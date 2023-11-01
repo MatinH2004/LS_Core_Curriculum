d0614eb2ad9a5d86ab24fc481c14ef1598277574
Matin Hassan Pour <matinhp04@gmail.com>
2023-10-31 18:17:31 -0700

    basic todo list

---
+++ LS_175/05_Project_Todos/todo.rb
@@ -0,0 +1,52 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :sessions_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View all of the lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Return an error message if the name is invalid.
+# Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "The list name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "The list name must be unique."   
+  end
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
