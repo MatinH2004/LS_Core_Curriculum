4f2b35a0151edfba3e0e3d26a359bcb6a26ecde1
Matin Hassan Pour <matinhp04@gmail.com>
2023-11-19 15:16:01 -0800

    reimplemented root to data_path

--- LS_175/08_Projects_File_Based_CMS/cms.rb
+++ LS_175/08_Projects_File_Based_CMS/cms.rb
@@ -9,7 +9,13 @@ configure do
   set :erb, :escape_html => true
 end
 
-root = File.expand_path("..", __FILE__)
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
 
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
@@ -27,14 +33,15 @@ def load_file_content(path)
 end
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index
 end
 
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.file?(file_path)
     load_file_content(file_path)
@@ -45,7 +52,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   @filename = params[:filename]
   @content = File.read(file_path)
@@ -54,7 +61,7 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   File.write(file_path, params[:content])
 
