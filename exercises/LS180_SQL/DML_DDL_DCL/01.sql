71cdcef2442609c10939f577b8c794a442e6206b
Matin Hassan Pour <matinhp04@gmail.com>
2024-01-02 12:50:09 -0800

    LS180 practice

---
+++ exercises/LS180_SQL/DML_DDL_DCL/01.sql
@@ -0,0 +1,17 @@
+/* Data Control Language: */
+
+GRANT
+REVOKE
+
+/* Data Manipulation Language: */
+
+SELECT
+INSERT
+UPDATE
+DELETE
+
+/* Data Definition Language: */
+
+CREATE
+ALTER
+DROP
---
+++ exercises/LS180_SQL/DML_DDL_DCL/02.sql
@@ -0,0 +1,6 @@
+/* Does the following query use DML or DDL? */
+
+SELECT column_name FROM my_table;
+
+/* SELECT is a part of DML sub-language, 
+since it manipulates the data and not the structure of the data */
