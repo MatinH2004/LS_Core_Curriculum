34645725cf6b23e6a830270b9a3330287e284bb9
Matin Hassan Pour <matinhp04@gmail.com>
2024-02-25 16:10:55 -0800

    standard schema used in app

--- LS_185/lesson_1/expense_tracker/schema.sql
+++ LS_185/lesson_1/expense_tracker/schema.sql
@@ -1,7 +1,6 @@
 CREATE TABLE expenses (
   id serial PRIMARY KEY,
-  amount numeric(6,2) NOT NULL,
+  amount numeric(6,2) NOT NULL CHECK (amount >= 0.01),
   memo text NOT NULL,
-  created_on date NOT NULL DEFAULT NOW(),
-  CONSTRAINT positive_amount CHECK (amount >= 0.01)
+  created_on date NOT NULL DEFAULT NOW()
 );
