f49cca85234aa445922226b1e63058a121cdd761
Matin Hassan Pour <matinhp04@gmail.com>
2024-04-15 23:13:07 -0700

    type coersion problems

---
+++ JS_210/01_JS_Basics/type_coersion_practice.js
@@ -0,0 +1,26 @@
+// 1.
+
+let x = '13';
+let y = 9;
+
+console.log(parseInt(x) + y);
+
+// 2.
+// x is coerced into an integer implicitly with *
+console.log(x * y);
+
+// 3. + 4.
+
+let npa = 212;
+let nxx = 555;
+let num = 1212;
+
+console.log(String(npa) + String(nxx) + String(num));
+
+// 5.
+
+let bool = true;
+let arr = [1, 2, 3];
+
+console.log(bool.toString());
+console.log(arr.toString());
