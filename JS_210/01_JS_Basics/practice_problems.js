d2e1844d1f7d50f56bfe76ef8ce05d61e1e82205
Matin Hassan Pour <matinhp04@gmail.com>
2024-04-15 22:37:32 -0700

    variables exercise

---
+++ JS_210/01_JS_Basics/practice_problems.js
@@ -0,0 +1,48 @@
+// 1.
+
+let numerator = 10;
+
+// 2.
+
+const DENOMINATOR = 2;
+
+// 3.
+
+let answer = numerator / DENOMINATOR;
+console.log(answer);
+
+// 4. 
+
+let incrementer = 1;
+
+// 5.
+
+let start = incrementer;
+
+// 6.
+
+let end;
+let difference;
+
+// 7.
+
+incrementer += 1;
+incrementer += 1;
+incrementer += 1;
+
+// 8.
+
+incrementer++;
+incrementer++;
+
+// 9.
+
+end = incrementer;
+difference = (end - start);
+console.log(end);
+console.log(difference);
+
+// 10.
+
+answer = (11 + 31) * 3;
+console.log(answer);
