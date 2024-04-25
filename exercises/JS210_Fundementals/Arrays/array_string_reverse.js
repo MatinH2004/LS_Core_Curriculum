ca0673c2b25933fa9532ce3b1dd4998436a04392
Matin Hassan Pour <matinhp04@gmail.com>
2024-04-25 13:58:02 -0700

    added my solution

--- exercises/JS210_Fundementals/Arrays/array_string_reverse.js
+++ exercises/JS210_Fundementals/Arrays/array_string_reverse.js
@@ -30,3 +30,32 @@ console.log(reverse([]));                // []
 const array = [1, 2, 3];
 console.log(reverse(array));             // [3, 2, 1]
 console.log(array);                      // [1, 2, 3]
+
+// My solution:
+
+function reverse(inputForReversal) {
+  if (Array.isArray(inputForReversal)) {
+    return reverseArray(inputForReversal);
+  }
+
+  return reverseArray(inputForReversal.split('')).join('');
+}
+
+function reverseArray(arr) {
+  let reversed = [];
+
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    reversed.push(arr[i]);
+  }
+
+  return reversed;
+}
+
+console.log(reverse('Hello'));           // "olleH"
+console.log(reverse('a'));               // "a"
+console.log(reverse([1, 2, 3, 4]));      // [4, 3, 2, 1]
+console.log(reverse([]));                // []
+
+const array = [1, 2, 3];
+console.log(reverse(array));             // [3, 2, 1]
+console.log(array);                      // [1, 2, 3]
