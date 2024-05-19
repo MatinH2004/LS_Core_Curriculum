f5a2230d05b3e0fcad2b7b1db975eea58e59bdff
Matin Hassan Pour <matinhp04@gmail.com>
2024-05-18 20:22:36 -0700

    Array exercise

--- exercises/JS210_Fundementals/Arrays/array_string_reverse.js
+++ exercises/JS210_Fundementals/Arrays/array_string_reverse.js
@@ -1,35 +1,35 @@
-function reverse(inputForReversal) {
-  if (Array.isArray(inputForReversal)) {
-    return reverseArray(inputForReversal);
-  } else {
-    return reverseString(inputForReversal);
-  }
-}
+// function reverse(inputForReversal) {
+//   if (Array.isArray(inputForReversal)) {
+//     return reverseArray(inputForReversal);
+//   } else {
+//     return reverseString(inputForReversal);
+//   }
+// }
 
-function reverseArray(inputForReversal) {
-  const reversed = [];
-  const length = inputForReversal.length;
+// function reverseArray(inputForReversal) {
+//   const reversed = [];
+//   const length = inputForReversal.length;
 
-  for (let i = 0; i < length; i += 1) {
-    reversed[length - i] = inputForReversal[i];
-  }
+//   for (let i = 0; i < length; i += 1) {
+//     reversed[length - i] = inputForReversal[i];
+//   }
 
-  return reversed;
-}
+//   return reversed;
+// }
 
-function reverseString(inputForReversal) {
-  const stringArray = inputForReversal.split(' ');
-  return reverseArray(stringArray).join(' ');
-}
+// function reverseString(inputForReversal) {
+//   const stringArray = inputForReversal.split(' ');
+//   return reverseArray(stringArray).join(' ');
+// }
 
-console.log(reverse('Hello'));           // "olleH"
-console.log(reverse('a'));               // "a"
-console.log(reverse([1, 2, 3, 4]));      // [4, 3, 2, 1]
-console.log(reverse([]));                // []
+// console.log(reverse('Hello'));           // "olleH"
+// console.log(reverse('a'));               // "a"
+// console.log(reverse([1, 2, 3, 4]));      // [4, 3, 2, 1]
+// console.log(reverse([]));                // []
 
-const array = [1, 2, 3];
-console.log(reverse(array));             // [3, 2, 1]
-console.log(array);                      // [1, 2, 3]
+// const array = [1, 2, 3];
+// console.log(reverse(array));             // [3, 2, 1]
+// console.log(array);                      // [1, 2, 3]
 
 // My solution:
 
@@ -49,6 +49,23 @@ function reverseArray(arr) {
   }
 
   return reversed;
+}
+
+// My new solution using reduceRight:
+
+function reverse(input) {
+  if (typeof input === 'string') {
+    return reverseArray(input.split('')).join('');
+  } else {
+    return reverseArray(input);
+  }
+}
+
+function reverseArray(arr) {
+  return arr.reduceRight((result, elem) => {
+    result.push(elem);
+    return result;
+  }, []);
 }
 
 console.log(reverse('Hello'));           // "olleH"
