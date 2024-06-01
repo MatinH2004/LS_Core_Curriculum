51aad617006f5844be46bbe456b5159a7767e7fe
Matin Hassan Pour <matinhp04@gmail.com>
2024-05-31 17:12:07 -0700

    Tweak Solution

--- LS_215/03_General_Problem_Solving_Approach/versionNumber.js
+++ LS_215/03_General_Problem_Solving_Approach/versionNumber.js
@@ -14,18 +14,22 @@
 - Output:
   - 1, -1, 0; or null invalid inputs
 - Rules:
-  - The rules to compare two version numbers
+  - if any inputs contain invalid characters, return null
+    - any characters other than digits and . are considered invalid
+  - Compare the two input versions
+    - if version1 > version2, return 1
+    - if version1 < version2, return -1
+    - if version1 === version2, return 0
+  - The rules to compare two version numbers:
     - start from the left most parts of the two version numbers
     - if the number part of the first version number is larger, then the first version number is larger
     - if the number part of the second version number is larger, then the first version number is smaller
     - if the number parts of both version numbers are the same, move to the next number part of the two version numbers
       - do the same comparison and decide which version number is larger
       - keep moving to the right until the result of the comparison is determined
+        - if we reach the end of only one number, pad that number with a 0 part
       - if we get to the last number part of the two version numbers and they're equal,
         then the two version numbers are equal
-  - if any inputs contain invalid characters, return null
-    - any characters other than digits and . are considered invalid
-    - a version number that begins or ends with a period or has more than one period in a row, is invalid.
 
 Example of number versioning system:
 
@@ -35,6 +39,9 @@ Example of number versioning system:
 Version:
 ['1', '18', '2']
 ['13', '37']
+
+- iterate through the version numbers, part by part
+- compare part numbers
 
 ---- Algorithm ----
 High-level:
@@ -54,67 +61,87 @@ Detailed:
 
 2. Split the input strings by the '.' character
 
-3. Iterate through each array until a larger value is found
-  - if one number is greater than the other, stop looping and return whichever is larger
-  - only continue iteration if previous numbers were equal
-  - if one version number contains more sections that the other:
-    - keep iterating:
-      - if following numbers are greater than 0, the number being iterated on is larger
-    - else both are equal
+3. Loop through the two version numbers' parts
+  - for each step, access one part from each version number
+  - if one version number runs out of parts, use 0
+  - compare the two parts
+    - if part1 < part2
+      - return -1
+    - if part1 > part2
+      - return 1
+    - if part1 === part2
+      - we move to the next pair of parts
 
-4. Return the correct return value based on whichever value was larger
+4. Return 0 if no other values were returned
 
 */
 
-// Need to implement: handling version numbers with period at beginning/end of strings.
+// Initial Solution:
+// function compareVersions(version1, version2) {
+//   if (!(validVersion(version1) && validVersion(version2))) return null;
 
-function compareVersions(version1, version2) {
-  if (!(validVersion(version1) && validVersion(version2))) return null;
+//   version1 = removeTrailingZeros(version1.split('.'));
+//   version2 = removeTrailingZeros(version2.split('.'));
 
-  version1 = removeTrailingZeros(version1.split('.'));
-  version2 = removeTrailingZeros(version2.split('.'));
+//   longestVersion = version1.length > version2.length ? version1 : version2;
 
-  longestVersion = version1.length > version2.length ? version1 : version2;
+//   for (let i = 0; i < longestVersion.length; i++) {
+//     if ((Number(version1[i]) > Number(version2[i])) ||
+//         (Number(version1[i]) > 0 && version2[i] === undefined)) return 1;
+//     if ((Number(version1[i]) < Number(version2[i])) ||
+//         (Number(version1[i]) === undefined && version2[i] > 0)) return -1;
+//   }
 
-  for (let i = 0; i < longestVersion.length; i++) {
-    if ((Number(version1[i]) > Number(version2[i])) ||
-        (Number(version1[i]) > 0 && version2[i] === undefined)) return 1;
-    if ((Number(version1[i]) < Number(version2[i])) ||
-        (Number(version1[i]) === undefined && version2[i] > 0)) return -1;
+//   return 0;
+// }
+
+// function removeTrailingZeros(version) {
+//   let result = [];
+//   let copyMode = false;
+//   version = [...version].reverse();
+
+//   for (let i = 0; i < version.length; i++) {
+//     if (version[i] !== '0') copyMode = true;
+//     if (copyMode) result.push(version[i]);
+//   }
+
+//   return result.reverse();
+// }
+
+// LS Solution:
+function compareVersions(versionA, versionB) {
+  if (!(validVersion(versionA) && validVersion(versionB))) return null;
+
+  let aParts = versionA.split('.').map(Number);
+  let bParts = versionB.split('.').map(Number);
+
+  let maxLength = Math.max(aParts.length, bParts.length);
+
+  for (let i = 0; i < maxLength; i += 1) {
+    let aValue = aParts[i] || 0;
+    let bValue = bParts[i] || 0;
+
+    if (aValue > bValue) {
+      return 1;
+    } else if (aValue < bValue) {
+      return -1;
+    }
   }
 
   return 0;
 }
 
 function validVersion(versionStr) {
-  return /^[0-9.]+$/.test(versionStr);
+  return /^[0-9]+(\.[0-9]+)*$/.test(versionStr);
 }
 
-function removeTrailingZeros(version) {
-  let result = [];
-  let copyMode = false;
-  version = [...version].reverse();
-
-  for (let i = 0; i < version.length; i++) {
-    if (version[i] !== '0') copyMode = true;
-    if (copyMode) result.push(version[i]);
-  }
-
-  return result.reverse();
-}
-
-console.log(compareVersions('1.1', '1.0') === 1);
-
-console.log(compareVersions('13.37', '1.18.2') === 1);
-
-console.log(compareVersions('0.1', '1') === -1);
-
-console.log(compareVersions('1.1', '1.2') === -1);
-
-console.log(compareVersions('1', '1.0') === 0);
-
-console.log(compareVersions('1.2', '1.2.0.0') === 0);
-
-console.log(compareVersions('1.1?', '1.0!') === null);
-
-console.log(compareVersions('hello', 'world') === null);
+console.log(compareVersions('1', '1'));            // 0
+console.log(compareVersions('1.1', '1.0'));        // 1
+console.log(compareVersions('2.3.4', '2.3.5'));    // -1
+console.log(compareVersions('1.a', '1'));          // null
+console.log(compareVersions('.1', '1'));           // null
+console.log(compareVersions('1.', '2'));           // null
+console.log(compareVersions('1..0', '2.0'));       // null
+console.log(compareVersions('1.0', '1.0.0'));      // 0
+console.log(compareVersions('1.0.0', '1.1'));      // -1
+console.log(compareVersions('1.0', '1.0.5'));      // -1
