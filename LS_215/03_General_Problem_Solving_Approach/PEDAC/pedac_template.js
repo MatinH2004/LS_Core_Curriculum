5e1f8a4a16d59154304a7b0d17813afb1ae30912
Matin Hassan Pour <matinhp04@gmail.com>
2024-06-21 17:21:46 -0700

    Updated PEDAC Template

--- LS_215/03_General_Problem_Solving_Approach/PEDAC/pedac_template.js
+++ LS_215/03_General_Problem_Solving_Approach/PEDAC/pedac_template.js
@@ -1,9 +1,6 @@
 /*
 Start time: 
 ---------- PROBLEM ----------
-RESTATE:
-
-
 INPUT:
 
 OUTPUT:
@@ -17,7 +14,3 @@ EXAMPLES:
 ---------- ALGORITHM --------
 
 */
-
-console.log(palindromeSieve([443, 12, 639, 121, 3232]));  //[443, 121, 3232]
-console.log(palindromeSieve([5, 55, 6655, 8787]));        // [5, 55, 6655, 8787]
-console.log(palindromeSieve([897, 89, 23, 54, 6197, 53342])); //  []
