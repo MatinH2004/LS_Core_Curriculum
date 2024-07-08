92ec36e2138403de1f2cae807de62cc8fe7a76f6
Matin Hassan Pour <matinhp04@gmail.com>
2024-07-08 10:40:55 -0700

    Updated Solution

--- exercises/JS210_Small_Problems/Advanced/mergeSortedLists.js
+++ exercises/JS210_Small_Problems/Advanced/mergeSortedLists.js
@@ -71,3 +71,17 @@ console.log(merge([1, 1, 3], [2, 2]));         // [1, 1, 2, 2, 3]
 console.log(merge([], [1, 4, 5]));             // [1, 4, 5]
 console.log(merge([1, 4, 5], []));             // [1, 4, 5]
 console.log(merge([], []));                    // []
+
+// LS Solution
+
+function merge(array1, array2) {
+  const copy1 = array1.slice();
+  const copy2 = array2.slice();
+  const result = [];
+
+  while (copy1.length > 0 && copy2.length > 0) {
+    result.push(copy1[0] <= copy2[0] ? copy1.shift() : copy2.shift());
+  }
+
+  return result.concat(copy1.length === 0 ? copy2 : copy1);
+}
