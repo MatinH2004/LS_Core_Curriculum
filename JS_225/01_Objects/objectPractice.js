0e21a24f1f42f0d746fdeeda515355af3062ca79
Matin Hassan Pour <matinhp04@gmail.com>
2024-07-28 23:19:12 -0700

    JS Object Practice Problems

---
+++ JS_225/01_Objects/objectPractice.js
@@ -0,0 +1,43 @@
+const invoices = {
+  unpaid: [],
+  paid: [],
+
+  add(name, amount) {
+    this.unpaid.push({
+      name,
+      amount
+    });
+  },
+
+  payInvoice(name) {
+    let unpaid = [];
+
+    this.unpaid.forEach(invoice => {
+      if (invoice['name'] === name) {
+        this.paid.push(invoice);
+      } else {
+        unpaid.push(invoice);
+      }
+    });
+
+    this.unpaid = unpaid;
+  },
+
+  totalDue() {
+    return this.unpaid.reduce((total, {amount}) => total + amount, 0);
+  },
+
+  totalPaid() {
+    return this.paid.reduce((total, {amount}) => total + amount, 0);
+  },
+};
+
+invoices.add('Due North Development', 250);
+invoices.add('Moonbeam Interactive', 187.50);
+invoices.add('Slough Digital', 300);
+
+invoices.payInvoice('Due North Development');
+invoices.payInvoice('Slough Digital');
+
+console.log(invoices.totalPaid()); // 550
+console.log(invoices.totalDue());  // 187.5
