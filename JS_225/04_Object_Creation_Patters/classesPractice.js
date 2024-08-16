63a4b28329cbf09a0e90248b50b30b4446fdca71
Matin Hassan Pour <matinhp04@gmail.com>
2024-08-15 17:24:41 -0700

    JS Class Practice

---
+++ JS_225/04_Object_Creation_Patters/classesPractice.js
@@ -0,0 +1,344 @@
+// class Cat {
+//   constructor(name = 'Kitty') {
+//     this.name = name;
+//   }
+
+//   greet() {
+//     console.log(`My name is ${this.name} and im a cat`);
+//   }
+
+//   rename(newName) {
+//     this.name = newName;
+//   }
+
+//   static genericGreeting() {
+//     console.log('Hello! I\'m a cat!');
+//   }
+// }
+
+// // let kitty = new Cat()
+
+// -------
+
+// class Rectangle {
+//   constructor(width, length) {
+//     this.width = width;
+//     this.length = length;
+//   }
+
+//   getWidth() {
+//     return this.width;
+//   }
+
+//   getLength() {
+//     return this.length;
+//   }
+
+//   getArea() {
+//     return this.width * this.length;
+//   }
+// }
+
+// class Square extends Rectangle {
+//   constructor(side) {
+//     super(side, side);
+//   }
+// }
+
+// -------
+
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   speaks() {
+//     return `${this.name} says meowwww.`;
+//   }
+// }
+
+// let fakeCat = Object.create(Cat.prototype);
+// console.log(fakeCat instanceof Cat); // logs true
+// console.log(fakeCat.hasOwnProperty('name')); // logs false
+// console.log(fakeCat.speaks()); // logs undefined says meowwww.
+
+// -------
+
+// class Pet {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// class Cat extends Pet {
+//   constructor(name, age, color) {
+//     super(name, age);
+//     this.color = color;
+//   }
+
+//   info() {
+//     return `My cat ${this.name} is ${this.age} years old and has ${this.color} fur.`
+//   }
+// }
+
+// let pudding = new Cat('Pudding', 7, 'black and white');
+// let butterscotch = new Cat('Butterscotch', 10, 'tan and white');
+
+// console.log(pudding.info());
+// console.log(butterscotch.info());
+
+// My cat Pudding is 7 years old and has black and white fur.
+// My cat Butterscotch is 10 years old and has tan and white fur.
+
+// -------
+
+// class Animal {
+//   constructor(name, age, legs, species, status) {
+//     this.name = name;
+//     this.age = age;
+//     this.legs = legs;
+//     this.species = species;
+//     this.status = status;
+//   }
+//   introduce() {
+//     return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
+//   }
+// }
+
+// class Cat extends Animal {
+//   constructor(name, age, status) {
+//     super(name, age, 4, 'cat', status);
+//   }
+
+//   introduce() {
+//     return super.introduce() + ' Meow meow!';
+//   }
+// }
+
+// let cat = new Cat("Pepe", 2, "happy");
+// console.log(cat.introduce() === "Hello, my name is Pepe and I am 2 years old and happy. Meow meow!");
+// // logs true
+
+// class Dog extends Animal {
+//   constructor(name, age, status, master) {
+//     super(name, age, 4, 'dog', status);
+//     this.master = master;
+//   }
+
+//   greetMaster() {
+//     return `Hello ${this.master}! Woof, woof!`
+//   }
+// }
+
+// -------
+
+// class Vehicle {
+//   constructor(make, model, wheels) {
+//     this.make = make;
+//     this.model = model;
+//     this.wheels = wheels;
+//   }
+
+//   info() {
+//     return `${this.make} ${this.model}`;
+//   }
+
+//   getWheels() {
+//     return this.wheels;
+//   }
+// }
+
+// class Car extends Vehicle {
+//   constructor(make, model) {
+//     super(make, model, 4);
+//   }
+// }
+
+// class Motorcycle extends Vehicle {
+//   constructor(make, model) {
+//     super(make, model, 2);
+//   }
+// }
+
+// class Truck extends Vehicle {
+//   constructor(make, model, payload) {
+//     super(make, model, 6);
+//     this.payload = payload;
+//   }
+// }
+
+// -------
+// Pseudo-classical style:
+
+// function Person(name) {
+//   this.name = name;
+// }
+
+// Person.prototype.greeting = function(text) {
+//   return `Hello, I'm ${this.name}. It's very nice to meet you.`;
+// }
+
+// function Shouter(name) {
+//   Person.call(this, name);
+// }
+
+// Shouter.prototype = Object.create(Person.prototype);
+// Shouter.prototype.greeting = function() {
+//   return Person.prototype.greeting.call(this).toUpperCase();
+// }
+
+// Using class style:
+
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   greeting(text) {
+//     return `Hello, I'm ${this.name}. It's very nice to meet you.`;
+//   }
+// }
+
+// class Shouter extends Person{
+//   greeting() {
+//     return super.greeting().toUpperCase();
+//   }
+// }
+
+// let person = new Person("Jane");
+// let shouter = new Shouter("Bob");
+
+// console.log(person.greeting()); // Hello, I'm Jane. It's very nice to meet you.
+// console.log(shouter.greeting()); // HELLO, I'M BOB. IT'S VERY NICE TO MEET YOU.
+
+// -------
+
+// class Pet {
+//   constructor(type, name) {
+//     this.type = type;
+//     this.name = name;
+//   }
+
+//   get info() {
+//     return `a ${this.type} named ${this.name}`;
+//   }
+// }
+
+// class Owner {
+//   constructor(name) {
+//     this.name = name;
+//     this.pets = [];
+//   }
+
+//   numberOfPets() {
+//     return this.pets.length;
+//   }
+
+//   adoptPet(pet) {
+//     this.pets.push(pet);
+//   }
+// }
+
+// class Shelter {
+//   constructor() {
+//     this.clients = [];
+//   }
+
+//   adopt(person, pet) {
+//     if (this.clients.indexOf(person) === -1) {
+//       this.clients.push(person);
+//     }
+//     person.adoptPet(pet);
+//   }
+
+//   printAdoptions() {
+//     this.clients.forEach(owner => {
+//       console.log(`\n${owner.name} has adopted the following pets:`);
+//       owner.pets.forEach(pet => {
+//         console.log(pet.info);
+//       });
+//     });
+//   }
+// }
+
+// let butterscotch = new Pet('cat', 'Butterscotch');
+// let pudding      = new Pet('cat', 'Pudding');
+// let darwin       = new Pet('bearded dragon', 'Darwin');
+// let kennedy      = new Pet('dog', 'Kennedy');
+// let sweetie      = new Pet('parakeet', 'Sweetie Pie');
+// let molly        = new Pet('dog', 'Molly');
+// let chester      = new Pet('fish', 'Chester');
+
+// let phanson = new Owner('P Hanson');
+// let bholmes = new Owner('B Holmes');
+
+// let shelter = new Shelter();
+// shelter.adopt(phanson, butterscotch);
+// shelter.adopt(phanson, pudding);
+// shelter.adopt(phanson, darwin);
+// shelter.adopt(bholmes, kennedy);
+// shelter.adopt(bholmes, sweetie);
+// shelter.adopt(bholmes, molly);
+// shelter.adopt(bholmes, chester);
+
+// shelter.printAdoptions();
+
+// console.log(`${phanson.name} has ${phanson.numberOfPets()} adopted pets.`);
+// console.log(`${bholmes.name} has ${bholmes.numberOfPets()} adopted pets.`);
+
+// P Hanson has adopted the following pets:
+// a cat named Butterscotch
+// a cat named Pudding
+// a bearded dragon named Darwin
+
+// B Holmes has adopted the following pets:
+// a dog named Molly
+// a parakeet named Sweetie Pie
+// a dog named Kennedy
+// a fish named Chester
+
+// P Hanson has 3 adopted pets.
+// B Holmes has 4 adopted pets.
+
+// -------
+
+class Banner {
+  constructor(message) {
+    this.message = message;
+  }
+
+  displayBanner() {
+    console.log([this.horizontalRule(), this.emptyLine(), this.messageLine(), this.emptyLine(), this.horizontalRule()].join("\n"));
+  }
+
+  horizontalRule() {
+    // return '+' + '-'.repeat(this.message.length + 2) + '+';
+    return `+-${'-'.repeat(this.message.length)}-+`
+  }
+
+  emptyLine() {
+    // return '|' + ' '.repeat(this.message.length + 2) + '|';
+    return `| ${' '.repeat(this.message.length)} |`
+  }
+
+  messageLine() {
+    return `| ${this.message} |`
+  }
+}
+
+let banner1 = new Banner('To boldly go where no one has gone before.');
+banner1.displayBanner();
+// +--------------------------------------------+
+// |                                            |
+// | To boldly go where no one has gone before. |
+// |                                            |
+// +--------------------------------------------+
+
+let banner2 = new Banner('');
+banner2.displayBanner();
+// +--+
+// |  |
+// |  |
+// |  |
+// +--+
+
