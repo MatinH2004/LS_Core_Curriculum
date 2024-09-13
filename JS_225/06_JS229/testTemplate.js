5834cfa01cf9cd351ca7b0218b665d6f978aa729
Matin Hassan Pour <matinhp04@gmail.com>
2024-09-13 10:43:33 -0700

    testing templates

---
+++ JS_225/06_JS229/testTemplate.js
@@ -0,0 +1,26 @@
+var $ol = document.querySelector("ol");
+
+function outputResult(message) {
+  var $li = document.createElement("li");
+  $li.innerText = message;
+  $ol.appendChild($li);
+  return $li;
+}
+
+function test(message, assertion) {
+  var $msg = outputResult(message),
+      passed = false;
+
+  try {
+    passed = assertion();
+  }
+  catch (e) {
+    passed = false;
+  }
+  $msg.setAttribute("class", passed ? "pass" : "fail");
+}
+
+// example
+test("assertion message", function() {
+  return 1 + 2 === 3;
+})
