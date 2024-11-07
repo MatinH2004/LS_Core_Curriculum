08899f31933f8166ba859dc3dc04f8dad387fee6
Matin Hassan Pour <matinhp04@gmail.com>
2024-11-07 12:37:54 -0800

    feat. invalid error handling + refactor

--- exercises/JS230_Front_End_Dev/Forms_Validation/form_validation_part_1/script.js
+++ exercises/JS230_Front_End_Dev/Forms_Validation/form_validation_part_1/script.js
@@ -1,66 +1,6 @@
-/*
-
-TODO:
-- fix form size when form error pops up (padding at bottom)
-- When sign up is clicked with all inputs empty, form submits.
-  - The error messages must show up in red, along with form error at top.
-*/
-
-function fieldErrorMsg(name, longerPasswordNeeded = false) {
-  name = name.split('_').map(str => str[0].toUpperCase() + str.slice(1)).join(' ');
-
-  if (longerPasswordNeeded) {
-    return `${name} must be at least 10 characters long.`;
-  }
-
-  if (name === 'Phone Number') {
-    return `Please enter a valid ${name}.`;
-  }
-
-  return `${name} is a required field.`;
-}
-
-function requiredFieldError(event) {
-  const inputElement = event.target;
-  const ddElement = event.target.closest('dd');
-  const errorSpan = ddElement.querySelector('.error_message');
-
-  if (['first_name', 'last_name', 'email'].includes(inputElement.id)) {
-    if (inputElement.value.trim().length === 0) {
-      inputElement.classList.add('invalid_field');
-      errorSpan.textContent = fieldErrorMsg(inputElement.id);
-    }
-  }
-
-  if ('password' === inputElement.id) {
-    if (inputElement.value.length === 0) {
-      inputElement.classList.add('invalid_field');
-      errorSpan.textContent = fieldErrorMsg(inputElement.id);
-    } else if (inputElement.value.length < 10) {
-      inputElement.classList.add('invalid_field');
-      errorSpan.textContent = fieldErrorMsg(inputElement.id, true);
-    }
-  }
-
-  if ('phone_number' === inputElement.id) {
-    if (!/^\d{3}\-\d{3}\-\d{4}$/.test(inputElement.value)) {
-      inputElement.classList.add('invalid_field');
-      errorSpan.textContent = fieldErrorMsg(inputElement.id);
-    }
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
-
-  // inputs
-  const firstName = document.querySelector('#first_name');
-  const lastName = document.querySelector('#last_name');
-  const email = document.querySelector('#email');
-  const password = document.querySelector('#password');
-  const phoneNumber = document.querySelector('#phone_number');
-
-  // const passwordMinLength = 10;
+  const inputs = document.querySelectorAll('input');
 
   form.addEventListener('focusout', requiredFieldError);
 
@@ -76,11 +16,71 @@ document.addEventListener('DOMContentLoaded', () => {
   }, true);
 
   form.addEventListener('submit', event => {
-    const invalidFields = document.querySelector('.invalid_field');
+    let formErrors = false;
 
-    if (invalidFields.length !== 0) {
-      document.querySelector('.form_errors').textContent = 'Fix errors before submitting.';
-      event.preventDefault();
-    }
+    inputs.forEach(input => {
+      requiredFieldError({ target: input });
+
+      if (input.classList.contains('invalid_field')) {
+        formErrors = true;
+      }
+    });
+
+    if (formErrors) {
+      event.preventDefault()
+      document.querySelector('.form_errors')
+              .textContent = 'Form cannot be submitted until errors are corrected.';
+    };
   });
 });
+
+function fieldErrorMsg(name, { invalidPassword = false, invalidEmail = false } = {}) {
+  if (invalidPassword) {
+    return `${name} must be at least 10 characters long.`;
+  }
+  if (invalidEmail || name === 'Phone Number') {
+    return `Please enter a valid ${name}.`;
+  }
+  return `${name} is a required field.`;
+}
+
+
+function requiredFieldError(event) {
+  const input = event.target;
+  const ddElement = event.target.closest('dd');
+  const errorSpan = ddElement.querySelector('.error_message');
+  const labelText = document.querySelector('label[for=' + input.id + ']').textContent;
+
+  if (['first_name', 'last_name', 'email'].includes(input.id)) {
+    if (input.value.trim().length === 0) {
+      input.classList.add('invalid_field');
+      errorSpan.textContent = fieldErrorMsg(labelText);
+    }
+  }
+
+  if ('email' === input.id) {
+    if (input.validity.patternMismatch) {
+      input.classList.add('invalid_field');
+      errorSpan.textContent = fieldErrorMsg(labelText, { invalidEmail: true });
+    }
+  }
+
+  if ('password' === input.id) {
+    if (input.value.length === 0) {
+      input.classList.add('invalid_field');
+      errorSpan.textContent = fieldErrorMsg(labelText);
+    } else if (input.validity.patternMismatch) {
+      input.classList.add('invalid_field');
+      errorSpan.textContent = fieldErrorMsg(labelText, { invalidPassword: true });
+    }
+  }
+
+  if ('phone_number' === input.id) {
+    if (input.value.length === 0) return;
+
+    if (input.validity.patternMismatch) {
+      input.classList.add('invalid_field');
+      errorSpan.textContent = fieldErrorMsg(labelText);
+    }
+  }
+}
